4cf5edacef51b968032eb94714166691f3b89cdc
Sergio Estrada <estrada.serg@gmail.com>
2021-12-10 02:59:28 -0600

    added images and changed photostitle

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Here are some Movie images';
+  image1 = 'https://starwarsblog.starwars.com/wp-content/uploads/2021/11/star-wars-the-high-republic-exclusive-editions_TALL-38763.jpg';
+  image2 = 'https://ntvb.tmsimg.com/assets/p8696131_b_h10_aa.jpg';
+  image3 = 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1565874716-51wharHn-3L.jpg';
 
   constructor() { }
 
