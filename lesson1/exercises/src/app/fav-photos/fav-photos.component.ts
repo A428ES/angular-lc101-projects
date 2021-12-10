a59ebcfa59b548657711486edcf7b7cacd162196
Sergio Estrada <estrada.serg@gmail.com>
2021-12-10 03:47:25 -0600

    done with exercise 1

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Here are some Movie images';
+  photosTitle = 'Photos/Images';
   image1 = 'https://starwarsblog.starwars.com/wp-content/uploads/2021/11/star-wars-the-high-republic-exclusive-editions_TALL-38763.jpg';
   image2 = 'https://ntvb.tmsimg.com/assets/p8696131_b_h10_aa.jpg';
   image3 = 'https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1565874716-51wharHn-3L.jpg';
