77fe9438217c7405bc995cb5bfa1973d2c1eb006
Sergio Estrada <estrada.serg@gmail.com>
2021-12-14 09:23:10 -0600

    finished all functionalities

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -12,8 +12,11 @@ export class AppComponent {
   height = 0;
   width = 0;
   message = 'Space shuttle ready for takeoff!';
-  takeOffEnabled = true;
-  clicked = false;
+  takeOffEnabled: boolean = true;
+  clicked: boolean = false;
+  leftr: number = 5;
+  topbottom: number = 10;
+  howManyPx: number = 37;
 
   handleTakeOff() {
     let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
@@ -25,42 +28,74 @@ export class AppComponent {
     }
  }
 
- boundCheck(location) {
-  if (location - 5 < 0 || location+25 > 100) {
-    this.color = "orange";
-  } else {
-    this.color = "blue";
-  }
+ land(){
+    this.color = 'green';
+    this.takeOffEnabled = true;
+    this.message = "Space shuttle landed";
+    this.clicked = false;
+    this.topbottom = 0;
+ }
+
+ abort(img){
+   let result = window.confirm("Are you sure you want to abort the mission?");
+
+   if(result){
+     this.land();
+
+     this.moveRocket(img, 'land');
+   }
+ }
+
+ boundCheck() {
+  if(this.takeOffEnabled === false && this.height === 0) {
+    this.land();
+   } else if(this.leftr >= 95 || this.leftr <= 5 || this.topbottom >= 90 || this.topbottom <= 10){
+    this.color = 'orange';
+   } else {
+     this.color = 'blue';
+   }
 }
 
  moveRocket(img, direction: string){
+  if(direction === 'land') {
+    this.topbottom = 10;
+    this.leftr = 5;
+    img.style.bottom = `calc(${this.topbottom}% - ${this.howManyPx}px)`;
+    img.style.left = `calc(${this.leftr}% - ${this.howManyPx}px)`;
+    
+    return;
+  }
 
   let moveInt: number = 5;
-  let movement: string;
-  
-  this.height = direction === 'up' ? this.height += 10000 : this.height;
-  this.height = direction === 'down' ? this.height -= 10000 : this.height;
 
-  if(direction === 'left' || direction === 'down'){
+  if(direction === 'right' || direction === 'down'){
     moveInt = -5;
   }
-  let vamanos
-  if(direction === 'left' || direction === 'right'){
 
-    movement = parseInt(img.style.left) + moveInt + '%';
-    if(parseInt(img.style.left) + moveInt >= 0 && (parseInt(img.style.left) + moveInt)+10 < 100 ){
-    img.style.left = movement;}
-    vamanos = parseInt(img.style.left);
-  } else {
-    movement = parseInt(img.style.bottom) + moveInt + '%';
-    if(parseInt(img.style.bottom) + moveInt >= 0 && (parseInt(img.style.bottom) + moveInt)+10 < 100 ){
-    img.style.bottom = movement;}
-    vamanos = parseInt(img.style.bottom);
+  if(direction === 'down'){
+    this.height -= 10000;
+  } else if(direction === 'up') {
+    this.height += 10000;
   }
 
-  vamanos += moveInt;  
+  if(direction === 'up' || direction === 'down'){
+    if((this.topbottom + moveInt) >= 10 && this.topbottom + moveInt <= 90){
+      this.topbottom += moveInt;
+      img.style.bottom = `calc(${this.topbottom}% - ${this.howManyPx}px)`;
+    }
+  } else {
+    if((this.leftr + moveInt) >= 5 && this.leftr + moveInt <= 95){
+    
+      if(this.leftr === 0){
+        moveInt += 10
+      }
 
-  this.boundCheck(vamanos);
+      this.leftr += moveInt;
+      img.style.left = `calc(${this.leftr}% - ${this.howManyPx}px)`;
+    }
+  }
+
+  this.boundCheck();
 }
 
 }
