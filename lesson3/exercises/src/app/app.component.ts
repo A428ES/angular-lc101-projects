1ae0a0730a0b7c14afaca28d2ce5994dbbde7c89
Sergio Estrada <estrada.serg@gmail.com>
2021-12-14 10:10:31 -0600

    fixed a few things

--- lesson3/exercises/src/app/app.component.ts
+++ lesson3/exercises/src/app/app.component.ts
@@ -17,39 +17,42 @@ export class AppComponent {
   leftr: number = 5;
   topbottom: number = 10;
   howManyPx: number = 37;
+  lastCSS: number;
 
-  handleTakeOff() {
+  handleTakeOff(img) {
     let result = window.confirm('Are you sure the shuttle is ready for takeoff?');
     if (result) {
        this.color = 'blue';
-       this.height = 10000;
+       this.height = 0;
        this.width = 0;
        this.message = 'Shuttle in flight.';
+
+       this.moveRocket(img, 'up');
+       this.moveRocket(img, 'up');
+       this.moveRocket(img, 'right');
     }
  }
 
- land(){
+ land(img){
     this.color = 'green';
     this.takeOffEnabled = true;
     this.message = "Space shuttle landed";
     this.clicked = false;
     this.topbottom = 0;
+
+    this.moveRocket(img, 'land');
  }
 
  abort(img){
    let result = window.confirm("Are you sure you want to abort the mission?");
 
    if(result){
-     this.land();
-
-     this.moveRocket(img, 'land');
+     this.land(img);
    }
  }
 
  boundCheck() {
-  if(this.takeOffEnabled === false && this.height === 0) {
-    this.land();
-   } else if(this.leftr >= 95 || this.leftr <= 5 || this.topbottom >= 90 || this.topbottom <= 10){
+  if(this.leftr >= 95 || this.leftr <= 5 || this.topbottom >= 90 || this.topbottom <= 10){
     this.color = 'orange';
    } else {
      this.color = 'blue';
@@ -68,20 +71,20 @@ export class AppComponent {
 
   let moveInt: number = 5;
 
-  if(direction === 'right' || direction === 'down'){
+  if(direction === 'left' || direction === 'down'){
     moveInt = -5;
-  }
-
-  if(direction === 'down'){
-    this.height -= 10000;
-  } else if(direction === 'up') {
-    this.height += 10000;
   }
 
   if(direction === 'up' || direction === 'down'){
     if((this.topbottom + moveInt) >= 10 && this.topbottom + moveInt <= 90){
       this.topbottom += moveInt;
       img.style.bottom = `calc(${this.topbottom}% - ${this.howManyPx}px)`;
+    
+      if(direction === 'down'){
+        this.height -= 10000;
+      } else if(direction === 'up') {
+        this.height += 10000;
+      }
     }
   } else {
     if((this.leftr + moveInt) >= 5 && this.leftr + moveInt <= 95){
@@ -94,8 +97,12 @@ export class AppComponent {
       img.style.left = `calc(${this.leftr}% - ${this.howManyPx}px)`;
     }
   }
-
+  this.lastCSS = this.leftr
   this.boundCheck();
+
+  if(this.takeOffEnabled === false && this.height === 0) {
+    return this.land(img);
+  } 
 }
 
 }
